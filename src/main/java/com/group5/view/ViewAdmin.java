e27942d212480cb19c523af550f40ced4a17c75e
coriandar <88608040+coriandar@users.noreply.github.com>
2023-04-30 22:21:02 +1200

    Remove book for user for release

--- src/main/java/com/group5/view/ViewAdmin.java
+++ src/main/java/com/group5/view/ViewAdmin.java
@@ -1,11 +1,7 @@
 package com.group5.view;
 
-import com.group5.account.Account;
-import com.group5.hotel.Room;
 import com.group5.system.HotelSystemAdmin;
 import com.group5.util.ParseInput;
-import java.util.Date;
-import java.util.List;
 import java.util.Scanner;
 
 public class ViewAdmin extends View {
@@ -24,9 +20,7 @@ public class ViewAdmin extends View {
 			System.out.println("3. View all users.");
 			System.out.println("4. View all bookings.");
 			System.out.println("5. Delete booking.");
-
 			System.out.println("6. Create booking for user.");
-
 			System.out.println("0. to QUIT");
 			System.out.println("------------------------------------");
 
@@ -36,9 +30,6 @@ public class ViewAdmin extends View {
 			else if (input == 3) viewAllUsers();
 			else if (input == 4) viewAllBookings();
 			else if (input == 5) deleteBooking(scan);
-
-			else if (input == 6) createBookingForUser(scan);
-
 			else if (input == 0) System.exit(0);
 		}
 		while(true);
@@ -54,25 +45,5 @@ public class ViewAdmin extends View {
 		System.out.println("Enter id of booking to delete:");
 		String bookingId = ParseInput.string(scan);
 		((HotelSystemAdmin) hotelSystem).deleteBookingByID(bookingId);
-	}
-
-	private void createBookingForUser(Scanner scan) {
-		System.out.println("Please enter username of account to book under: ");
-		String username = ParseInput.string(scan);
-		Account user = ((HotelSystemAdmin) hotelSystem).getUserAccountByUsername(username);
-
-		if(user != null) {
-			Date begin = super.getBeginDate(scan);
-			Date end = super.getEndDate(scan, begin);
-			super.printBookingPeriod(begin, end);
-			List<Room> rooms = getBookingRooms(scan, begin, end);
-
-			if (!rooms.isEmpty()) {
-//				hotelSystem.makeBooking(begin, end, rooms, user);
-				((HotelSystemAdmin)hotelSystem).makeBooking(begin, end, rooms, user);
-			}
-			else System.out.println("No rooms valid rooms selected, no bookings made.");
-		}
-		else System.out.println("User not found");
 	}
 }
