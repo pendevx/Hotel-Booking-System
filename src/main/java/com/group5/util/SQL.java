5bf39eb1ee18fca76c283d895d874abe56db78f7
coriandar <88608040+coriandar@users.noreply.github.com>
2023-06-02 15:48:58 +1200

    Rename to SQL

---
+++ src/main/java/com/group5/util/SQL.java
@@ -0,0 +1,45 @@
+package com.group5.util;
+
+public class SQL {
+	//////////////////////////////////////////////////
+	// CREATE TABLE
+	//////////////////////////////////////////////////
+	public static String createAccountTable() {
+		return "";
+	}
+
+	public static String createCredentialTable() {
+		return "";
+	}
+
+	public static String createUsernameTable() {
+		return "";
+	}
+
+	public static String createBookingTable() {
+		return "";
+	}
+
+	public static String createHotelTable() {
+		return "";
+	}
+
+	public static String dropTable(String tableName) {
+		return "DROP TABLE " + tableName;
+	}
+
+	//////////////////////////////////////////////////
+	// UPDATE TABLE
+	//////////////////////////////////////////////////
+	public static void updateAccountTable() {
+	}
+
+	public static void updateCredentialTable() {
+	}
+
+	public static void updateUsernameTable() {
+	}
+
+	public static void updateBookingTable() {
+	}
+}
--- src/main/java/com/group5/util/SQLStatment.java
+++
@@ -1,5 +0,0 @@
-package com.group5.util;
-
-public class SQLStatment {
-	
-}
